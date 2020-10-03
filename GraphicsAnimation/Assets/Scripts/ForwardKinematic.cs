793c8f53f223bf55b150689e9ba226ee0ec8e7e8
nicholasbrennanmart <nicholas.brennanmart@mymail.champlain.edu>
2020-10-03 19:32:37 -0400

    4 kinds of interpolation

--- GraphicsAnimation/Assets/Scripts/ForwardKinematic.cs
+++ GraphicsAnimation/Assets/Scripts/ForwardKinematic.cs
@@ -10,7 +10,69 @@ using UnityEngine;
 
 public class ForwardKinematic : MonoBehaviour
 {
-    
 
+    public HierarchicalPose samplePose;
 
+    public void Nearest(HierarchicalPose hp, HierarchicalPose hp2, float t)
+    {
+
+        for (int i = 0; i < samplePose.currentPose.Length; i++)
+        {
+
+            if (t < .5f)
+            {
+
+                samplePose.currentPose[i].translation = hp.currentPose[i].translation;
+                samplePose.currentPose[i].orientation = hp.currentPose[i].orientation;
+                samplePose.currentPose[i].scale = hp.currentPose[i].scale;
+            }
+
+            else
+            {
+
+                samplePose.currentPose[i].translation = hp2.currentPose[i].translation;
+                samplePose.currentPose[i].orientation = hp2.currentPose[i].orientation;
+                samplePose.currentPose[i].scale = hp2.currentPose[i].scale;
+            }
+        }
+    }
+
+    public void Linear(HierarchicalPose hp, HierarchicalPose hp2, float t)
+    {
+
+        for (int i = 0; i < samplePose.currentPose.Length; i++)
+        {
+
+                samplePose.currentPose[i].translation = (1 - t) * hp.currentPose[i].translation + hp2.currentPose[i].translation * t;
+                samplePose.currentPose[i].orientation = (1 - t) * hp.currentPose[i].orientation + hp2.currentPose[i].orientation * t;
+                samplePose.currentPose[i].scale = (1 - t) * hp.currentPose[i].scale + hp2.currentPose[i].scale * t;
+        }
+    }
+
+    public void Step(HierarchicalPose hp, HierarchicalPose hp2, float t)
+    {
+
+        for (int i = 0; i < samplePose.currentPose.Length; i++)
+        {
+
+            samplePose.currentPose[i].translation = hp.currentPose[i].translation;
+            samplePose.currentPose[i].orientation = hp.currentPose[i].orientation;
+            samplePose.currentPose[i].scale = hp.currentPose[i].scale;
+
+        }
+    }
+
+    public void Smoothstep(HierarchicalPose hp, HierarchicalPose hp2, float t)
+    {
+
+        for (int i = 0; i < samplePose.currentPose.Length; i++)
+        {
+
+            float alpha = t * t * (3 - 2 * t);
+
+            samplePose.currentPose[i].translation = hp.currentPose[i].translation * alpha + (hp2.currentPose[i].translation * (1 - alpha));
+            samplePose.currentPose[i].orientation = hp.currentPose[i].orientation * alpha + (hp2.currentPose[i].orientation * (1 - alpha));
+            samplePose.currentPose[i].scale = hp.currentPose[i].scale * alpha + (hp2.currentPose[i].scale * (1 - alpha));
+        }
+    }
 }
