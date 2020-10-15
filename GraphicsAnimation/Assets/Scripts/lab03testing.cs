14e18a1d0e7c310351bd120fca2f239ae24c966e
nicholasbrennanmart <nicholas.brennanmart@mymail.champlain.edu>
2020-10-15 14:59:17 -0400

    start creating test scene

---
+++ GraphicsAnimation/Assets/Scripts/lab03testing.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lab03testing : MonoBehaviour
+{
+
+    public enum Blend
+    {identity, construct, copy, invert, merge, nearest, linear, cubic,
+        split, scale, triangular, binear, bilinear, bicubic};
+    public Blend blend;
+
+    public enum Direction { play, reverse, pause };
+    public Direction direction;
+
+    public float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(direction == Direction.play)
+        {
+
+            t += Time.deltaTime;
+
+            if(t >= 1)
+            {
+
+                t = 0;
+            }
+        }
+
+        if (direction == Direction.reverse)
+        {
+
+            t -= Time.deltaTime;
+
+            if (t <= 0)
+            {
+
+                t = 1;
+            }
+        }
+
+        if (direction == Direction.pause)
+        {
+
+            //nothing
+        }
+    }
+}
