3acc7b88a4a36fa31eb897dbc1420f3a2c670595
nicholasbrennanmart <nicholas.brennanmart@mymail.champlain.edu>
2020-11-06 11:05:07 -0500

    add display

---
+++ GraphicsAnimation/Assets/Scripts/lab4display.cs
@@ -0,0 +1,34 @@
+﻿/*
+File name: lab4display.cs
+Purpose:  this display the information for lab 4
+Contributors: Nick Brennan-Martin and Bradley Chamberlain
+Collaborated on one PC
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lab4display : MonoBehaviour
+{
+
+    public Text mode;
+    public Text conditional;
+    public GameObject player;
+    public GameObject transition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mode.text = player.GetComponent<ForwardKinematic>().mode;
+        //conditional = transform.GetComponent<Conditional>().currentClip;
+    }
+}
